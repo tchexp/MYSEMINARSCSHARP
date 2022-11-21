18b739bee187e105e5a6702ebaef5f1e1fd2f503
tchexp <tchexp@mail.ru>
2022-11-22 00:23:39 +0300

    commit homework 2(8)

---
+++ HOMEWORK/HW2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+/*
+int GetSecondDigit(int num)
+{
+    int res = (num / 10) % 10;
+    return res;
+}
+Console.Write("Please input three digit number N = ");
+int numN = Convert.ToInt32(Console.ReadLine());
+int result = GetSecondDigit(numN);
+Console.WriteLine($"Second digit of {numN} -> {result}");
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+/*
+int GetThirdDigit(int num)
+{
+    int var = num;   
+    int digits = 0;
+    while (var > 0 )
+    {
+        digits++;
+                                   
+        if ((var / 1000 == 0) && (var / 100 != 0 )) //укороченное число является трехзначным
+        {
+            int res = var % 10;                     //возвращаем младший разряд
+            return res ;
+        }
+        var = var / 10;                             //укорачивание числа с младших разрядов
+    }
+    return -1;
+}
+Console.Write("Please input number N = ");
+int numN = Convert.ToInt32(Console.ReadLine());
+int result = GetThirdDigit(numN);
+if (result == -1) 
+{
+    Console.WriteLine($"Number {numN} has no third digit");
+}
+else Console.WriteLine($"Third digit of {numN} -> {result}");
+*/
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+/*
+bool IsWeekend(int num)
+{
+    if ((num == 6)||(num == 7)) return true;
+    else return false;
+}
+Console.Write("Please enter day of the week = ");
+int day = Convert.ToInt32(Console.ReadLine());
+bool result = IsWeekend(day);
+Console.WriteLine($"Day of the week is weekend -> {result}");
+*/
---
+++ SEMINARS/Seminar2/Program.cs
@@ -0,0 +1,108 @@
+﻿//Задача 1. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//78 -> 8
+//12-> 2
+//85 -> 8
+/*
+int FindBiggestDigit (int num)
+{
+int ed = num % 10;
+int dec = num / 10;
+if (ed > dec) return ed;
+else return dec;
+}
+
+int random = new Random().Next(10,100);
+int result = FindBiggestDigit(random);
+Console.WriteLine($" Random number {random} has bigest digit {result}");
+*/
+
+//2 Задача.Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+//456 -> 46
+//782 -> 72
+//918 -> 98
+/*
+int CutNumber (int num)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+    int res = sot * 10 + ed;
+    return res;
+}
+int random = new Random().Next(100,1000);
+int result = CutNumber(random);
+Console.WriteLine($" Result of circumsize of {random} will be {result}");
+*/
+
+//Задача 3.Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. 
+//Если второе число не кратно числу первому, то программа выводит остаток от деления.
+//34, 5 -> не кратно, остаток 4
+//16, 4  -> кратно
+
+void IsDiv (int num)
+{
+
+}
+
+
+
+//4 Задача.Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+14  ->  нет
+46  ->  нет
+161 ->  да
+
+
+
+bool IsDiv (int num1)
+{
+    if (( num1 % 7 == 0 ) && ( num1 % 23 == 0 ))
+
+    {
+        return true;
+    
+    }
+     else
+    {
+      return false;
+    }
+}
+int A = 162;
+bool B =IsDiv (A);
+
+Console.WriteLine(B);
+
+
+//int CutNumber (int num) 
+//{
+   // int ed = num % 10;
+   // int sot = num / 100;
+    //
+   // int res = sot * 10 + ed;
+//    return res;
+//}
+
+//int random = new Random().Next(100,1000);
+//int newNumber = CutNumber(random);
+//Console.WriteLine($"New version of {random} is {newNumber}");/
+// void IsDiv (int num1, int num2)
+// {
+
+//     if (num2 % num1 == 0)
+//     {
+//         Console.WriteLine ($"number {num2} {num1} -> devisble");
+    
+//     }
+//     else 
+
+//     {
+//        Console.WriteLine ($"number {num2} {num1} ->not devisble {num2 % num1}");
+//     }
+// }
+// int A = 5;
+// int B = 35;
+// IsDiv (A, B);
+// int random = new Random().Next(100,1000);
+// int newNumber = CutNumber(random);
+// Console.WriteLine($"New version of {random} is {newNumber}");/

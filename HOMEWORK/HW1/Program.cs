0240549064f2c7d8cde75dd679c96a4d83dc2485
tchexp <tchexp@mail.ru>
2022-11-21 12:38:16 +0300

    Homework 7

---
+++ HOMEWORK/HW1/Program.cs
@@ -0,0 +1,41 @@
+﻿//Выполнил Захаров Николай
+
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+/*
+Console.Write("Please input number a = ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input number b = ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA > numB)   Console.Write($" -> max = {numA}       a > b");
+if (numA < numB)   Console.Write($" -> max = {numB}       b > a");
+if (numA == numB)  Console.Write($" -> max = {numB}       a = b");
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+/*
+Console.Write("Please input number ");
+int numN = Convert.ToInt32(Console.ReadLine());
+if (numN % 2 == 0)   Console.Write($"Number {numN} is Even ");
+else Console.Write($"Number {numN} is Odd ");
+*/
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+/*
+Console.Write("Please input number N = ");
+int numN = Convert.ToInt32(Console.ReadLine());
+int cnt = 2;
+while (cnt <= numN) 
+{
+    Console.Write($"{cnt}; ");
+    cnt = cnt + 2;
+}
+*/
---
+++ SEMINARS/Seminar1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+/*Console.WriteLine("Please input number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);*/
+
+
+/*Console.WriteLine("Please input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int sqr = num * num;
+Console.Write($"Square of {num} is {sqr}");*/
+
+
+//даны два числа - проверить является ли первое число квадратом второго
+//
+/*Console.WriteLine("Please input a");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please input b");
+int numB = Convert.ToInt32(Console.ReadLine());
+int sqrB = numB * numB;
+if (numA == sqrB)  
+{
+    Console.Write($"Number a = {numA} is a sqare of b {numB}");
+}
+else 
+{
+    Console.Write($"Number a = {numA} is not a sqare of b {numB}");
+}*/
+
+//на вход принимает одно число N, а на выходе показывает все целые числа в промежутке от -N до N
+
+/*Console.WriteLine("Please input N");
+int numN = Convert.ToInt32(Console.ReadLine());
+int crnt = numN * (-1); 
+while (crnt < numN )
+{
+    Console.Write($"{crnt}; ");
+    crnt++;
+}*/
+
+//получает на ввод трехзначное число и на выходе показывает последнюю цифру
+Console.WriteLine("Please input N");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.Write($" last digit is {numN % 10}; ");
+

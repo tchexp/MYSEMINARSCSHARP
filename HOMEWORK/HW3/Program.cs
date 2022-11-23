3c183a2a08f9c20cc19855ddabca66c552c2802a
tchexp <tchexp@mail.ru>
2022-11-23 10:42:09 +0300

    Homework 3

---
+++ HOMEWORK/HW3/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// int CountDigits(int num) //Вспомогоательный метод счтечик цифр в числе
+// {
+//     return Convert.ToInt32(Math.Floor(Math.Log10(num) + 1));
+// }
+
+// bool IsPalindrome(int num)  //проверяет любое число - является ли оно палиндромом
+// {   
+//     int number = num;
+//     int length = CountDigits(num);
+//     for (int i = 0; i < length / 2; i++)
+//     {
+//         int firstDgt = number / Convert.ToInt32(Math.Pow(10,CountDigits(number)-1)); //первая цифра числа
+//         int lastDgt = number % 10;                                                   //последняя цифра числа
+//         if (firstDgt != lastDgt) return false;
+//         number = number % Convert.ToInt32(Math.Pow(10,CountDigits(number)-1)) / 10; //"обрезали" число с двух концов
+//     }
+//     return true;
+// }
+
+// int number = 0;
+// while (number != 99925)
+// {
+//     Console.Write ("Please enter N = ");
+//     number = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine($"Number {number} is palindrome -> {IsPalindrome(number)}");
+// }
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+// double GetDistance (int x1, int y1, int z1, int x2, int y2, int z2)
+// {
+//     double distance = Math.Round(Math.Sqrt(Math.Pow(x1-x2,2)
+//                                    +Math.Pow(y1-y2,2)
+//                                    +Math.Pow(z1-z2,2)    
+//                                   ),2
+//                          );
+//     return distance;
+// }
+
+// Console.Write ("Please enter x of point A ");
+// int xA = Convert.ToInt32(Console.ReadLine());
+// Console.Write ("Please enter y of point A ");
+// int yA = Convert.ToInt32(Console.ReadLine());
+// Console.Write ("Please enter z of point A ");
+// int zA = Convert.ToInt32(Console.ReadLine());
+// Console.Write ("Please enter x of point B ");
+// int xB = Convert.ToInt32(Console.ReadLine());
+// Console.Write ("Please enter y of point B ");
+// int yB = Convert.ToInt32(Console.ReadLine());
+// Console.Write ("Please enter z of point B ");
+// int zB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Distance between A({xA}, {yA}, {zA}) and B({xB}, {yB}, {zB}) is -> { GetDistance(xA,yA,zA,xB,yB,zB)}");
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// void GetCubes(int num)
+// {
+//     Console.Write ($"Cubes of {num} -> 1 ");
+//     for (int i = 2; i <= num; i++)
+//     {
+//         Console.Write ($", {Math.Pow(i,3)}");
+//     }
+// }
+
+// Console.Write ("Please enter number N = ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// GetCubes(number);
---
+++ SEMINARS/Seminar3/Program.cs
@@ -0,0 +1,66 @@
+﻿// //Задание1.  Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+// //четверти (x и y).
+
+// int FindQuart(double x, double y)
+// {
+//     if ((x > 0)&&(y > 0 )) return 1;
+//     if ((x < 0)&&(y > 0 )) return 2;
+//     if ((x < 0)&&(y < 0 )) return 3;
+//     if ((x > 0)&&(y < 0 )) return 4;
+//     return 0;
+// }
+
+// Console.Write ("Введите число x = ");
+// int x = Convert.ToDouble(Console.ReadLine());
+// // Console.Write ("Введите координату y = ");
+// // double y = Convert.ToDouble(Console.ReadLine());
+// // int result = FindQuart(x, y);
+// // Console.WriteLine($"The quarter number of our point {result}");
+
+
+// //Задача 2.Напишите программу, которая по заданному номеру четверти, 
+// //показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// void FindCoordinates(int quart)
+// {
+//     if ((quart < 1)||(quart > 4)) Console.WriteLine("Non existent quarter");
+//     if (quart == 1) Console.WriteLine("Range of x - positive, range of y - positive");
+//     if (quart == 2) Console.WriteLine("Range of x - negative, range of y - positive");
+//     if (quart == 3) Console.WriteLine("Range of x - negative, range of y - negative");
+//     if (quart == 4) Console.WriteLine("Range of x - positive, range of y - negative");            
+// }
+// /*
+// Console.Write ("Input quart = ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// FindCoordinates(quart: x);
+// */
+
+// //Задача 3. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// //A (3,6); B (2,1) -> 5,09
+// //A (7,-5); B (1,-1) -> 7,21
+// //взять квадратный корень из суммы квадратов разности его координат
+
+// double GetLength(double x1, double y1, double x2, double y2)
+// {
+//     double l = Math.Sqrt()
+// }
+
+// for (int i = 0; i < length; i++)
+// {
+    
+// }
+
+// //Задача 4. Напишите программу которая принимает на вход число (N) и выдает на консоль квадраты чисел от 1 до N
+
+
+void SqrOl(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        Console.WriteLine(i * i);
+    }
+}
+SqrOl(5);
+
+Console.Write ("Введите число N = ");
+int x = Convert.ToDouble(Console.ReadLine());

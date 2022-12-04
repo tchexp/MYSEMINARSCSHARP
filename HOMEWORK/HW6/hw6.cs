14e144bb6d93d506edd2ed6db3e0b975df2e0815
tchexp <tchexp@mail.ru>
2022-12-04 13:14:57 +0300

    HW8 Separate files Done 2

--- HOMEWORK/HW6/hw6.cs
+++ HOMEWORK/HW6/hw6.cs
@@ -1,4 +1,5 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл 
+//пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
@@ -44,29 +45,30 @@
 
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
-//заданных уравнениями y = k1 * x + b1, 
-//                     y = k2 * x + b2; 
+//заданных уравнениями 
+//y = k1 * x + b1, 
+//y = k2 * x + b2; 
 
 //значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-// string FindIntersection(int b1, int k1, int b2, int k2)
-// {
-//     float tan = k2 - k1;
-//     if (tan == 0) return ("Lines are parallel, no solution");
-//     float x = (b1 - b2) / tan;
-//     float y = (k2 * b1 - k1 * b2) / tan;
+string FindIntersection(int b1, int k1, int b2, int k2)
+{
+    float tan = k2 - k1;
+    if (tan == 0) return ("Lines are parallel, no solution");
+    float x = (b1 - b2) / tan;
+    float y = (k2 * b1 - k1 * b2) / tan;
 
-//     return ($"({Math.Round(x, 2)}, {Math.Round(y, 2)})");
-// }
+    return ($"({Math.Round(x, 2)}, {Math.Round(y, 2)})");
+}
 
-// Console.Write("Please input k1 -> ");
-// int k1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Please input b1 -> ");
-// int b1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Please input k2 -> ");
-// int k2 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Please input b2 -> ");
-// int b2 = Convert.ToInt32(Console.ReadLine());
-// string output = FindIntersection(k1, b1, k2, b2);
-// Console.WriteLine($"k1 = {k1}, b1 = {b1}, k2 = {k2}, b2 = {b2} -> {output}");
+Console.Write("Please input b1 -> ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input k1 -> ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input b2 -> ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input k2 -> ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+string output = FindIntersection(b1, k1, b2, k2);
+Console.WriteLine($"k1 = {b1}, b1 = {k1}, k2 = {b2}, b2 = {k2} -> {output}");
